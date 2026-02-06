2b320daf817be5188e0764b9cb7aab826e32c670
shipalibhandary <shipalibhandary@gmail.com>
2026-02-06 12:00:49 +0530

    add additional screens to RootLayout for navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,6 +8,10 @@ export default function RootLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="splash"/>
+      <Stack.Screen name="login"/>
+      <Stack.Screen name="home"/>
+      <Stack.Screen name="quiz"/>
+      <Stack.Screen name="profile"/>
     </Stack>
   );
 }
